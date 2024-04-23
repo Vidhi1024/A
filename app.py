b9d87ea271cd6e4fee5e1eb925097ec62f539f1b
Vidhi <118298119+Vidhi1024@users.noreply.github.com>
2024-04-23 15:58:39 +0000

    increment function

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app=Flask(__name__)
+counter = 0
 
 @app.route('/')
-def Aa():
-  return '0'
+def index():
+    global counter
+    counter += 1
+    return f'{counter}'
 
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
